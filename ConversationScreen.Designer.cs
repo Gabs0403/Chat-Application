46b9c622d8ed08c81eddc1364c06be48f4d03e60
Henrique Baggio <134702403+IqueBaggio28@users.noreply.github.com>
2024-11-19 23:53:47 -0500

    Add files via upload

---
+++ ConversationScreen.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ChatEngine
+{
+    partial class ConversationScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            fontDialog1 = new FontDialog();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 54);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(105, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 298);
+            button1.Name = "button1";
+            button1.Size = new Size(206, 38);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 355);
+            button2.Name = "button2";
+            button2.Size = new Size(206, 38);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 412);
+            button3.Name = "button3";
+            button3.Size = new Size(206, 38);
+            button3.TabIndex = 5;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 632);
+            button4.Name = "button4";
+            button4.Size = new Size(206, 38);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(12, 526);
+            button5.Name = "button5";
+            button5.Size = new Size(206, 38);
+            button5.TabIndex = 8;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(12, 469);
+            button6.Name = "button6";
+            button6.Size = new Size(206, 38);
+            button6.TabIndex = 7;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 579);
+            button7.Name = "button7";
+            button7.Size = new Size(206, 38);
+            button7.TabIndex = 6;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // fontDialog1
+            // 
+            
+            // 
+            // ConversationScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1447, 832);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "ConversationScreen";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ConversationScreen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private FontDialog fontDialog1;
+    }
+}
